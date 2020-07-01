43d6b0b21313172f04f9380890b320a89b7e57df
akloc@github.com <akloc@github.com>
2020-06-30 23:33:01 -0400

    Project Structure setup

---
+++ swiftTraining/swiftTraining/1 The Basics.swift
@@ -0,0 +1,17 @@
+//
+//  1 The Basics.swift
+//  swiftTraining
+//
+//  Created by Andy Kloc on 6/30/20.
+//  Copyright © 2020 Andy Kloc. All rights reserved.
+//
+
+import Foundation
+
+class TheBasics
+{
+    init()
+    {
+        print("Hi from TheBasics.swift")
+    }
+}
---
+++ swiftTraining/swiftTraining/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftTraining
+//
+//  Created by Andy Kloc on 6/30/20.
+//  Copyright © 2020 Andy Kloc. All rights reserved.
+//
+
+import Foundation
+
+// no "new".
+let theBasics = TheBasics()
